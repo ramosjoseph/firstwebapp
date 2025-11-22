ff79eab92bbc066cc064bb286f5e3fba2dce61f4
Joseph Ramos <112776305+ramosjoseph@users.noreply.github.com>
2025-11-22 14:12:04 +0000

    Update message

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Cloud!!!");
-  res.write("Welcome to my page.");
+  res.write("Hello!");
+  res.write(" Welcome to my web app ⸜(｡˃ ᵕ ˂ )⸝♡.");
   res.end();
 });
 
